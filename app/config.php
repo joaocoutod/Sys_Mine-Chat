de18403bd0a70ebf21f0b7335dce8a0a4c691f9e
João Lucas <54899969+joaocoutod@users.noreply.github.com>
2021-01-02 14:00:22 -0400

    Update config.php

--- app/config.php
+++ app/config.php
@@ -3,17 +3,11 @@ function conexao(){
     try {
         
         //mysql local
-        /*
         $db_host = "localhost";
         $db_db = "chat";
         $db_user = "root";
         $db_pass = "";
-        */
         
-        $db_host = "localhost";
-        $db_db = "id15813679_chat";
-        $db_user = "id15813679_chatadm";
-        $db_pass = "n8{?u@jd?Z>[Psp9";
 
         $conn = new PDO("mysql:host=$db_host;dbname=$db_db;","$db_user","$db_pass");
         
